fb7a76780c7879e1694879d3dade5028f916c640
yagorodilov <yar.gorodilov@ya.ru>
2024-10-12 10:21:14 +0300

    EMERALD-7
    Пуш новой ветки

--- bot.py
+++ bot.py
@@ -1,14 +1,17 @@
 import telebot
-# from telebot import types
+from telebot import types
 import var
 from var import token
 import logging
-
 import get_ip
+import time
 
 token = var.token
-
 bot = telebot.TeleBot(f'{token}')
+
+#TODO: Настроить логирование (EMERALD-6)
+
+
 
 @bot.message_handler(commands=['start'])
 def handle_start(message):
@@ -18,8 +21,17 @@ def handle_start(message):
   button2 = types.KeyboardButton('Soon')
   keyboard.add(button1, button2)
 
-  # Отправка сообщения с клавиатурой
-  bot.reply_to(message, 'Привет! Я бот.', reply_markup=keyboard)
+  # Запись пинкода в переменную
+  #TODO: Настроить безопасность данных бота (EMERALD-7)
+  bot.send_message(message.chat.id, 'Доброго времени суток! Я Эмеральд!')
+  time.sleep(0.7)
+  bot.send_message(message.chat.id, 'Пожалуйста, введите Ваш PIN-код')
+  def echo_message(message):
+    messages = []
+    text = message.text
+    messages.append(text)
+
+    print(messages)
 
 @bot.message_handler(content_types=['text', 'photo', 'sticker'])
 def handle_message(message):
